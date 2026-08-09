9b794604cff0a67791d582af01c9fc1b131189b8
Anton Lysenko <probold777@gmail.com>
2026-08-09 18:42:23 -0400

    mobile standalone app fix(icon and layout)

--- client/src/app/+html.tsx
+++ client/src/app/+html.tsx
@@ -18,10 +18,12 @@ export default function Root({ children }: PropsWithChildren) {
         <meta name="apple-mobile-web-app-title" content={APP_NAME} />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="theme-color" content={THEME_COLOR} />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.webmanifest" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="1024x1024" href="/apple-touch-icon.png" />
       </head>
       <body {...bodyAttributes}>
         {children}
